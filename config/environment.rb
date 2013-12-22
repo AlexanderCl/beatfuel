afcbe7b449005780ddad837422a21f625cba6b9f
Alexander Claeys <me@alexanderclaeys.be>
2013-12-22 20:06:54 +0100

    V1.1

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Beatfuel::Application.initialize!
+
+# Variables
+ENV['FACEBOOK_APP_ID'] = 'devuser'
+ENV['FACEBOOK_SECRET'] = 'devpass'
