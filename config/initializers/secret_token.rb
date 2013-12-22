21948c3eace01e7460953a0ca68d9b6c4560623e
Alexander Claeys <me@alexanderclaeys.be>
2013-12-22 11:47:01 +0100

    'V1'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Beatfuel::Application.config.secret_key_base = '41a3ee1c2b4c920252e5af9a687e41c2849be923321bfee659066f96fc5174a8a3b36b138e48245a80f8c83cfee27bf001e73127d492982d55ec6611f68cc46c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Beatfuel::Application.config.session_store :cookie_store, key: '_beatfuel_session'
