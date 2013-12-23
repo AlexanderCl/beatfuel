3b4d78561c423326529751a90d57bba86a46722f
semvc <sem.vande.capelle@student.howest.be>
2013-12-23 11:08:04 +0100

    modified:   .idea/workspace.xml 	new file:   app/assets/javascripts/facebook.js.coffee 	modified:   app/models/user.rb 	modified:   app/views/layouts/application.html.erb 	modified:   app/views/shared/_nav.html.erb 	modified:   config/environments/development.rb 	modified:   config/initializers/omniauth.rb 	modified:   config/routes.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 
-  Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
-  end
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :facebook, '542813182473001', 'cb10cb28d260acb56f7cd2c04f48343e'   , {:provider_ignores_state => true}
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Beatfuel::Application.routes.draw do
   get "pages/home"
   root 'pages#home'
 
+  get 'auth/:provider/callback' => 'sessions#create'
+  get 'auth/failure' => redirect('/')
+  get 'signout' => 'sessions#destroy', as: 'signout'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
